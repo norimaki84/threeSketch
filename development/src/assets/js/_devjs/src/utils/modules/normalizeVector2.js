0593d513c30accc8e22b35b9a608af177791c8c1
Norikazu Teraguchi <norimaki.tera@gmail.com>
2018-01-09 19:36:41 +0900

    [add]

---
+++ development/src/assets/js/_devjs/src/utils/modules/normalizeVector2.js
@@ -0,0 +1,9 @@
+/**
+ * 正規化関数
+ * @param vector
+ */
+
+export default function(vector) {
+	vector.x = (vector.x / document.body.clientWidth) * 2 - 1;
+	vector.y = - (vector.y / window.innerHeight) * 2 + 1;
+};
