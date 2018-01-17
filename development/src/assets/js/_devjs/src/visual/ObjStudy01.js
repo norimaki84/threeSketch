12fdf4efce3776b06f5d8aa88d0cd1a82e53b871
Norikazu Teraguchi <norimaki.tera@gmail.com>
2018-01-17 23:20:30 +0900

    [modify]bananaシェーダー追加

--- development/src/assets/js/_devjs/src/visual/ObjStudy01.js
+++ development/src/assets/js/_devjs/src/visual/ObjStudy01.js
@@ -7,7 +7,6 @@
  */
 
 import Entry from '../Core/Entry';
-import loadTexture from '../utils/modules/loadTexture';
 
 require('../../libs/loaders/OBJLoader');
 
@@ -24,43 +23,32 @@ export default class ObjStudy01 extends Entry {
     this.width = document.body.clientWidth;
     this.height = document.body.clientHeight;
 
+    // 基本セット
     this.camera = null;
     this.renderer = null;
     this.scene = null;
 		this.pointsLight = null;
 		this.ambientLight = null;
-		this.loader = null;
-		this.group = null;
+		this.uniforms = null;
 
-		this.select = 0;
-		this.tweenTime = 0.8;
-		this.planeSize = 256;
-		this.dx = 250;
-		this.dz = 250;
-		this.show = 2;
-		this.deltaRotation = 15;
-		this.half = 0;
-		this.tweening = false;
+		// オフスクリーンレンダリングで使用
+		this.baseScene = null;
+		this.baseCamera = null;
+		this.baseLight = null;
+		this.baseGeometry = null;
+		this.baseMaterial = null;
+		this.baseMesh = null;
+		this.renderTarget = null;
 
     this.createCamera = this._createCamera.bind(this);
     this.createScene = this._createScene.bind(this);
 		this.createRenderer = this._createRenderer.bind(this);
     this.createLight = this._createLight.bind(this);
-
-
+		this.offScreenEvent = this._offScreenEvent.bind(this);
     this.loadModel = this._loadModel.bind(this);
-
+    this.utilEvent = this._utilEvent.bind(this);
     this.onResize = this._onResize.bind(this);
 		this.Update = this._Update.bind(this);
-
-		this.loadTextureEvent = this._loadTextureEvent.bind(this);
-		this.mouseWheel = this._mouseWheel.bind(this);
-
-		this.texsArray = {
-			img01 : '../../../../assets/resource/img/sample01.jpg',
-			img02 : '../../../../assets/resource/img/sample02.jpg',
-			img03 : '../../../../assets/resource/img/sample03.jpg',
-		};
 
   }
 
@@ -74,21 +62,11 @@ export default class ObjStudy01 extends Entry {
     this.createRenderer();
 		this.createLight();
 
+		this.offScreenEvent();
+
 		this.loadModel();
 
-		// カルーセル群を追加するグループを作成
-		// this.group = new THREE.Object3D();
-		// this.group.position.y = this.planeSize/2;
-		// this.scene.add(this.group);
-
-		// this.loadTextureEvent();
-
-		//軸の長さ１０００
-		let axis = new THREE.AxesHelper(1000);
-		this.scene.add(axis);
-		let controls;
-		controls = new THREE.OrbitControls(this.camera);
-		controls.autoRotate = true;
+		this.utilEvent();
 
 		this.Update();
 
@@ -104,8 +82,8 @@ export default class ObjStudy01 extends Entry {
   _createCamera() {
 
     this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1.0, 2000);
-    this.camera.position.x = 0;
-    this.camera.position.y = 0;
+    this.camera.position.x = 5;
+    this.camera.position.y = 10;
     this.camera.position.z = 10;
 
     // this.camera.lookAt(new THREE.Vector3(0,0,0));
@@ -160,6 +138,53 @@ export default class ObjStudy01 extends Entry {
 	}
 
 	/**
+	 * オフスクリーンレンダリングイベント
+	 * @private
+	 */
+	_offScreenEvent() {
+
+		// オフスクリーンレンダリングの描画処理（renderTarget への描画用）
+		this.baseScene = new THREE.Scene();
+
+		// オフスクリーンレンダリングの描画処理用カメラ
+		this.baseCamera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
+		this.baseCamera.position.z = 1;
+
+		// オフスクリーンレンダリング用ライトを追加
+		this.baseLight = new THREE.DirectionalLight(new THREE.Color(0xffffff), 1);
+		this.baseLight.position.set(0, 10, 20);
+		this.baseScene.add(this.baseLight);
+
+		// ベース用のマテリアルとジオメトリ
+		this.baseGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+		//
+		this.uniforms = {
+			u_time: { type: "f", value: 1.0 },
+			u_resolution: { type: "v2", value: new THREE.Vector2() },
+		};
+		this.baseMaterial = new THREE.RawShaderMaterial({
+			uniforms: this.uniforms,
+			vertexShader: require('../../../../glsl/banana.vert'),
+			fragmentShader: require('../../../../glsl/banana.frag'),
+			side: THREE.DoubleSide,
+			transparent: true
+		});
+		this.baseMesh = new THREE.Mesh(this.baseGeometry, this.baseMaterial);
+		this.baseScene.add(this.baseMesh);
+
+
+		// オフスクリーンレンダリング用
+		this.renderTarget = new THREE.WebGLRenderTarget(1, 1, {
+			magFilter: THREE.NearestFilter,
+			minFilter: THREE.NearestFilter,
+			wrapS: THREE.ClampToEdgeWrapping,
+			wrapT: THREE.ClampToEdgeWrapping
+		});
+
+	}
+
+	/**
 	 * 3Dモデル読み込み・初期化
 	 * @private
 	 */
@@ -184,32 +209,48 @@ export default class ObjStudy01 extends Entry {
 		let onError = function ( xhr ) {
 		};
 
+		// let textureLoader = new THREE.TextureLoader( manager );
+		// let texture = textureLoader.load( '../../../../assets/resource/img/Banana.jpg' );
 
 		this.loader = new THREE.OBJLoader( manager );
 		this.loader.load( '../../../../assets/resource/model/banana03.obj', function ( object ) {
 			object.traverse( function ( child ) {
-				// window.console.log('object', object.children[0].material);
-				let mat = new THREE.RawShaderMaterial({
-					// uniforms: this.uniforms,
-					vertexShader: require('../../../../glsl/base.vert'),
-					fragmentShader: require('../../../../glsl/base.frag'),
+
+				let material = new THREE.MeshPhongMaterial({
+					map: that.renderTarget, //renderTargetをテクスチャとして渡す
+					wireframe: false
 				});
-				// object.children[0].material = mat;
-				window.console.log('child', child);
-				window.console.log('child.material', child.material);
+				window.console.log(material.map);
+				object.children[0].material = material;
+
 				if ( child instanceof THREE.Mesh ) {
 					// child.material.map = texture;
 				}
-			} );
-			// object.position.set(0.0, -95.0, 0.0);
+
+			});
+
 			object.position.set(0.0, 0.0, 0.0);
 			object.rotation.set(0.0, 0.0, 0.0);
-			// object.scale.set(0.6, 0.6, 0.6);
 			let scaleVal = 1.0;
 			object.scale.set(scaleVal, scaleVal, scaleVal);
 			that.scene.add( object );
 		}, onProgress, onError );
 
+	}
+
+	/**
+	 *
+	 * @private
+	 */
+	_utilEvent (){
+		//軸の長さ
+		let axis = new THREE.AxesHelper(1000);
+		this.scene.add(axis);
+
+		// カメラ
+		let controls;
+		controls = new THREE.OrbitControls(this.camera);
+		controls.autoRotate = true;
 	}
 
   /**
@@ -218,6 +259,12 @@ export default class ObjStudy01 extends Entry {
    */
   _Update() {
 
+		this.uniforms.u_time.value += 0.02;
+
+		// オフスクリーンレンダリング
+		this.renderer.render(this.baseScene, this.baseCamera, this.renderTarget);
+
+		//
 		this.renderer.render(this.scene, this.camera);
 
     requestAnimationFrame( () => {
@@ -232,63 +279,12 @@ export default class ObjStudy01 extends Entry {
   _onResize() {
 		this.canvas.width = document.body.clientWidth;
     this.canvas.height = document.body.clientHeight;
-		// this.plane.mesh.material.uniforms.resolution.value.set(document.body.clientWidth, document.body.clientHeight);
 
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-
-
-	/**
-	 * 画像をロード
-	 * @private
-	 */
-	_loadTextureEvent() {
-
-		// const texLoader = new THREE.TextureLoader();
-		// texLoader.crossOrigin = '*'; //クロスドメイン読み込みの許可
-		// texLoader.load('../../../../assets/resource/img/shibuya01.jpg',
-		// 	texture => { // onLoad
-		// 		// const geometry = new THREE.BoxGeometry(1, 1, 1);
-		// 		// const material = new MeshBasicMaterial({ map: texture });
-		// 		// const cube = new THREE.Mesh(geometry, material);
-		// 		// scene.add(cube);
-		// 		// ...
-		// 		window.console.log('完了');
-		// 	},
-		// 	xhr => { // onProgress
-		// 		window.console.log('');
-		// 	},
-		// 	xhr => { // onError
-		// 		// ...
-		// 	}
-		// );
-
-		loadTexture(this.texsArray, function () {
-
-			window.console.log('リソース読み込み完了');
-
-		});
-
-	}
-
-	/**
-	 * カルーセルのマウスホイールイベント
-	 * @private
-	 */
-	_mouseWheel(event) {
-		let delta = 0;
-		if (!event) event = window.event;   /* IE  */
-		if (event.wheelDelta) {             /* IE, Opera. */
-			delta = event.wheelDelta/120;
-		} else if (event.detail) {          /* Mozilla */
-			delta = -event.detail;
-		}
-
-		if(delta && tweening===false) gotoDir(delta/Math.abs(delta));
-	}
 
 	setEvents() {
 
