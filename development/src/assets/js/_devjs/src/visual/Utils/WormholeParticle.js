36fb71002c17b4907064cdedd9a2e8500f1db7da
Norikazu Teraguchi <norimaki.tera@gmail.com>
2018-01-29 22:41:39 +0900

    [modify]

--- development/src/assets/js/_devjs/src/visual/Utils/WormholeParticle.js
+++ development/src/assets/js/_devjs/src/visual/Utils/WormholeParticle.js
@@ -14,9 +14,22 @@ export default class WormholeParticle extends Entry {
 
   constructor(scene, burst, time) {
 
+  	// 引数
+		this.scene = scene;
+		this.burst = burst;
+		this.time = time;
+
 		this.radius = Math.random()*0.002 + 0.0003;
-		this.geom = this.icosahedron;
+		// this.geom = this.icosahedron;
+		this.geom = null;
 		this.random = Math.random();
+
+		this.cube = null;
+		this.sphere = null;
+		this.icosahedron = null;
+
+		this.createGeometry = this._createGeometry.bind(this);
+		this.Update = this._Update.bind(this);
 
     this.init();
   }
@@ -26,9 +39,42 @@ export default class WormholeParticle extends Entry {
    */
   init(){
 
-    this.createCameraObject();
+  	let that = this;
+
+		if(this.random > 0.9){
+			that.geom = this.cube;
+		} else if(this.random > 0.8){
+			that.geom = this.sphere;
+		}
+
+		let range = 50;
+		if(this.burst){
+			this.color = new THREE.Color("hsl("+(time / 50)+",100%,60%)");
+		} else {
+			let offset = 180;
+			this.color = new THREE.Color("hsl("+(Math.random() * range + offset)+",100%,80%)");
+		}
+		let mat = new THREE.MeshPhongMaterial({
+			color: this.color,
+			shading:THREE.FlatShading
+		});
 
   }
 
+	/**
+	 * ジオメトリ作成
+	 * @private
+	 */
+	_createGeometry() {
+
+	}
+
+	/**
+	 * 更新
+	 * @private
+	 */
+	_Update() {
+
+	}
 
 }
