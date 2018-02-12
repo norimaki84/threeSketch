126087572117f3ac95ae02fee1d2f34ff12cda90
Norikazu Teraguchi <norimaki.tera@gmail.com>
2018-02-12 23:44:30 +0900

    [modify]ClockTextureクラス移植中

--- development/src/assets/js/_devjs/src/visual/Utils/ClockTexture.js
+++ development/src/assets/js/_devjs/src/visual/Utils/ClockTexture.js
@@ -21,16 +21,56 @@ export default class ClockTexture extends Entry {
 		this.width = this.canvas.width = width;
 		this.height = this.canvas.height = height;
 
+		this.getTime = this._getTime.bind(this);
+		this.render = this._render.bind(this);
+
     this.init();
   }
 
   /**
    * 初期化
    */
-  init(){
+  init() {
 
 
   }
 
+	/**
+	 *
+	 * @private
+	 */
+	_getTime() {
+
+		let date = new Date();
+
+		let hours = date.getHours();
+		let minutes = date.getMinutes();
+		let seconds = date.getSeconds();
+
+		if (hours.toString().length === 1) hours = `0${hours}`;
+		if (minutes.toString().length === 1) minutes = `0${minutes}`;
+		if (seconds.toString().length === 1) seconds = `0${seconds}`;
+
+		return `${hours}:${minutes}:${seconds}`;
+
+	}
+
+	/**
+	 *
+	 * @private
+	 */
+	_render() {
+
+		this.ctx.fillStyle = '#111';
+		this.ctx.fillRect(0, 0, this.width, this.height);
+		this.ctx.fillStyle = '#fff';
+		this.ctx.font = '120px Arial';
+		this.ctx.textAlign = 'center';
+		this.ctx.fillText(this.getTime(), this.width / 2, this.height / 2 + 30);
+		let img = this.ctx.getImageData(0, 0, this.width, this.height);
+
+		return new THREE.Texture(img);
+
+	}
 
 }
