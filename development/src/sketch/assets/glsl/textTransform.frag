370b9e5e023db6fbab87a7b03cc1d37f717d6c25
Norikazu Teraguchi <norimaki.tera@gmail.com>
2019-07-18 17:14:37 +0900

    [add]  キャンバステクスチャーで文字列を表示する

--- development/src/sketch/assets/glsl/textTransform.frag
+++ development/src/sketch/assets/glsl/textTransform.frag
@@ -18,8 +18,8 @@ void main(){
 //    vec4 dest = texture2D(texture, vec2(vUv.x, vUv.y + offset));
 
     vec2 zure = vec2(sin(vUv.y * 12.0 + u_time * 0.1) * 0.02, 0.0);
-//    vec4 dest = texture2D(texture, vUv + zure + offset);
-    vec4 dest = texture2D(texture, vUv);
+    vec4 dest = texture2D(texture, vUv + zure + offset);
+//    vec4 dest = texture2D(texture, vUv);
 
     gl_FragColor = dest;
 
--- development/src/sketch/assets/js/_devjs/src/visual/TextEffect.js
+++ development/src/sketch/assets/js/_devjs/src/visual/TextEffect.js
@@ -8,7 +8,7 @@
 
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
-import CaptureText from "./Utils/CaptureText";
+// import CaptureText from "./Utils/CaptureText";
 
 'use strict';
 
@@ -25,11 +25,15 @@ export default class TextEffect {
 
 		this.capText = null;
 
+		this.posX = null;
+		this.posY = null;
+		this.mesh = null;
+
 		/**
 		 * カメラ作成
 		 * @type {PerspectiveCamera}
 		 */
-		this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 1.0, 10000);
+		this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 1.0, 1000);
 
 		/**
 		 * レンダラー作成
@@ -50,6 +54,9 @@ export default class TextEffect {
 		 */
 		this.scene = new THREE.Scene();
 
+		this.uniforms = null;
+
+		this.createLabel = this._createLabel.bind(this);
 		this.createLight = this._createLight.bind(this);
     this.onResize = this._onResize.bind(this);
 		this.Update = this._Update.bind(this);
@@ -73,13 +80,19 @@ export default class TextEffect {
 		this.createLight();
 
 		// テキスト、フォントサイズ、フォント種類、フォントの色
-		this.capText = new CaptureText('No', 256, 'sans-serif', '000000');
-		this.scene.add(this.capText.planeTexture);
+		// this.capText = new CaptureText('No', 256, 'sans-serif', '000000');
+		// this.scene.add(this.capText.planeTexture);
+		// let x = window.innerWidth / 2 - 300;
+		this.posX = this.width / 2;
+		this.posY = this.height / 2 - 300;
+		this.mesh = this.createLabel('Norimaki.jp', this.posX, this.posY, 0, 100, '000', 'bold', 'sans-serif', 'ffff00', 50);
+		this.scene.add(this.mesh);
 
-		window.console.log(this.capText.planeTexture);
+		// window.console.log(this.capText.planeTexture);
 
 		this.Update();
 
+		this.onResize();
 		window.addEventListener('resize', this.onResize, false);
 
   }
@@ -96,24 +109,87 @@ export default class TextEffect {
 
 	}
 
+	/**
+	 * キャンバステクスチャーを生成
+	 * @param text							: 文字列
+	 * @param x									: 文字の位置(X)
+	 * @param y									: 文字の位置(Y)
+	 * @param z									: 文字の位置(Z)
+	 * @param size							: サイズ
+	 * @param color							: 色
+	 * @param weight						: ウェイト
+	 * @param font							: フォントネーム
+	 * @param backGroundColor		: 背景色
+	 * @param backgroundMargin	: 余白
+	 */
+	_createLabel(text, x, y, z, size, color, weight, font, backGroundColor, backgroundMargin) {
+
+		if(!backgroundMargin)
+			backgroundMargin = 50;
+
+		let canvas = document.createElement('canvas');
+
+		let context = canvas.getContext('2d');
+		context.font = weight + ' ' + size + 'px ' + font;
+
+		let textWidth = context.measureText(text).width;
+
+		canvas.width = textWidth + backgroundMargin;
+		canvas.height = size + backgroundMargin;
+		context = canvas.getContext('2d');
+		context.font = weight + ' ' + size + 'px ' + font;
+
+		if(backGroundColor) {
+			context.fillStyle = '#' + backGroundColor;
+			context.fillRect(canvas.width / 2 - textWidth / 2 - backgroundMargin / 2, canvas.height / 2 - size / 2 - +backgroundMargin / 2, textWidth + backgroundMargin, size + backgroundMargin);
+		}
+
+		context.textAlign = 'center';
+		context.textBaseline = 'middle';
+		context.fillStyle = '#' + color;
+		context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+		let texture = new THREE.CanvasTexture(canvas);
+		texture.minFilter = THREE.LinearFilter;
+		texture.needsUpdate = true;
+
+		// ユニフォーム変数
+		this.uniforms = {
+			texture: { type: 't', value: texture },
+			u_time: { type: 'f', value: 1.0 },
+		};
+
+		// let material = new THREE.MeshBasicMaterial({
+		// 	map : texture
+		// });
+
+		let material = new THREE.RawShaderMaterial({
+			uniforms: this.uniforms,
+			vertexShader: require('../../../../glsl/textTransform.vert'),
+			fragmentShader: require('../../../../glsl/textTransform.frag'),
+			side: THREE.DoubleSide,
+			transparent: true,
+		});
+
+		let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(canvas.width, canvas.height), material);
+		// mesh.overdraw = true;
+		mesh.doubleSided = true;
+		mesh.position.x = x - canvas.width;
+		mesh.position.y = y - canvas.height;
+		mesh.position.z = z;
+
+		return mesh;
+	}
+
   /**
    * 更新
    * @private
    */
   _Update() {
 
-		// オフスクリーンレンダリング
-		// this.renderer.render(this.capText.baseScene, this.capText.baseCamera, this.capText.renderTarget);
-		this.renderer.setRenderTarget(this.capText.renderTarget);
-		this.renderer.render(this.capText.baseScene, this.capText.baseCamera);
-		this.renderer.setRenderTarget(null);
-		this.renderer.clear();
-
-		this.capText.drawText();
-
-		this.capText.uniforms.u_time.value += 1.0;
-
 		this.renderer.render(this.scene, this.camera);
+
+		this.uniforms.u_time.value += 1.0;
 
     requestAnimationFrame(() => {
       this.Update();
@@ -127,12 +203,17 @@ export default class TextEffect {
 	 */
   _onResize() {
 
+		this.width = document.body.clientWidth;
+		this.height = document.body.clientHeight;
+
 		this.canvas.width = document.body.clientWidth;
     this.canvas.height = document.body.clientHeight;
 
-    this.camera.aspect = window.innerWidth / window.innerHeight;
+    // this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.aspect = this.width / this.height;
     this.camera.updateProjectionMatrix();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(this.width, this.height);
 
   }
 
